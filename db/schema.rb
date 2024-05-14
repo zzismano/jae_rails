6edfefb3bd978b7b03443d837d31a040a205b371
Carla Valdivia <valdivia.carla@gmail.com>
2024-05-14 10:55:28 -0300

    [IMPRTNT] changed database from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_13_181141) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "subcategories"
