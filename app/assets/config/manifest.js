44586e881a90f3eb3aa4f8b1d1fbe4f6105e5d32
Carla Valdivia <valdivia.carla@gmail.com>
2024-05-17 15:55:43 -0300

    Removed bootstrap link and installed Bootstrap and Sass into project

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
+//= link application.css
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link popper.js
+//= link bootstrap.min.js
