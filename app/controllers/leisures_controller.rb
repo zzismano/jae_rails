0462e7d64f89a8381d5ea2d8dec5093c271e2c1e
Carla Valdivia <valdivia.carla@gmail.com>
2024-05-17 14:24:43 -0300

    Add edit action on leisures controller

--- app/controllers/leisures_controller.rb
+++ app/controllers/leisures_controller.rb
@@ -1,4 +1,5 @@
 class LeisuresController < ApplicationController
+  before_action :set_leisure, only: [:edit, :update]
 
   def index
     @leisures = policy_scope(Leisure)
@@ -15,10 +16,24 @@ class LeisuresController < ApplicationController
     @leisure.save
   end
 
+  def edit
+    
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
 
   def leisure_params
-    params.require(:leisure).permit(:category_id, :venue_id, :picture, :genre, :link, :title, :subtitle, :director, :country, :description, :features, :min_age, :duration, :time, :start_date, :end_date)
+    params.require(:leisure).permit(:category_id, :venue_id, :photo, :genre, :link, :title, :subtitle, :director, :country, :description, :features, :min_age, :duration, :time, :start_date, :end_date)
+  end
+
+  def set_leisure
+    @leisure = Leisure.find(params[:id])
   end
 
 end
