1559764eb83f54d0815e4139f232414afc32c4b3
Carla Valdivia <valdivia.carla@gmail.com>
2024-05-20 14:38:35 -0300

    Update seeds to include user_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,46 +13,48 @@ admin1.save!
 admin2 = User.new(email: 'pedro@jae.com', password: 123456, admin: true)
 admin2.save!
 
+users = [admin1, admin2]
+
 puts "Creating venues..."
 
-cinemark = Venue.new(name: 'Cinemark', address: 'Praia de Botafogo', capacity: 100, website: 'www.cinemark.com.br')
+cinemark = Venue.new(name: 'Cinemark', address: 'Praia de Botafogo', capacity: 100, website: 'www.cinemark.com.br', user: users.sample)
 
 cinemark.save!
 
-uci = Venue.new(name: 'UCI NYCC', address: 'Avenida das Américas 5000', capacity: 150, website: 'www.uci-nycc.com.br')
+uci = Venue.new(name: 'UCI NYCC', address: 'Avenida das Américas 5000', capacity: 150, website: 'www.uci-nycc.com.br', user: users.sample)
 
 uci.save!
 
-poeira = Venue.new(name: 'Teatro Poeira', address: 'R. São João Batista, 104', capacity: 80, website: 'www.teatropoeira.com.br')
+poeira = Venue.new(name: 'Teatro Poeira', address: 'R. São João Batista, 104', capacity: 80, website: 'www.teatropoeira.com.br', user: users.sample)
 
 poeira.save!
 
 puts "Creating category...."
 
-filme = Category.new(name: 'Filme', subcategories: ["Drama", "Horror", "Comedy"])
+filme = Category.new(name: 'Filme', subcategories: ["Drama", "Horror", "Comedy"], user: users.sample)
 
 filme.save!
 
-teatro = Category.new(name: 'Teatro', subcategories: ["Drama", "Horror", "Comedy"])
+teatro = Category.new(name: 'Teatro', subcategories: ["Drama", "Horror", "Comedy"], user: users.sample)
 
 teatro.save!
 
 puts "Creating leisure..."
 
-leisure = Leisure.new(category: filme, venue: uci, link: 'www.guerracivil.com.br', title: "Guerra Civil", subtitle: 'A Guerra de dois mundos', director: 'Francisco Padilha', country: 'USA', description: 'O fime retrata a guerra de dois mundos e a cobertura jornalistica realizada pela grande mídia', features: 'Wagner Moura', min_age: 16, duration: 2, start_date: '10/05/2024', end_date: '20/06/2024')
+leisure = Leisure.new(category: filme, venue: uci, link: 'www.guerracivil.com.br', title: "Guerra Civil", subtitle: 'A Guerra de dois mundos', director: 'Francisco Padilha', country: 'USA', description: 'O fime retrata a guerra de dois mundos e a cobertura jornalistica realizada pela grande mídia', features: 'Wagner Moura', min_age: 16, duration: 2, start_date: '10/05/2024', end_date: '20/06/2024', user: users.sample)
 
 leisure.save!
 
-leisure2 = Leisure.new(category: teatro, venue: poeira, link: 'www.teatropoeira.com.br', title: "Sonata de Outono", subtitle: '', director: 'Marieta Severo', country: 'BR', description: 'Uma sonata dedicada à relação de mãe e filho', features: 'Marieta Severo, Andrea Beltrao', min_age: 18, duration: 2, start_date: '10/05/2024', end_date: '20/06/2024')
+leisure2 = Leisure.new(category: teatro, venue: poeira, link: 'www.teatropoeira.com.br', title: "Sonata de Outono", subtitle: '', director: 'Marieta Severo', country: 'BR', description: 'Uma sonata dedicada à relação de mãe e filho', features: 'Marieta Severo, Andrea Beltrao', min_age: 18, duration: 2, start_date: '10/05/2024', end_date: '20/06/2024', user: users.sample)
 
 leisure2.save!
 
 puts "Creating genre..."
 
-artsy = Genre.new(name: 'Artsy')
+artsy = Genre.new(name: 'Artsy', user: users.sample)
 artsy.save!
 
-cartaz = Genre.new(name: 'Em cartaz')
+cartaz = Genre.new(name: 'Em cartaz', user: users.sample)
 cartaz.save!
 
 puts "Creating leisure_genre..."
