a87c7ce32c5cbc8eb7e4d4c6ea3cc65ab6710016
Pedro Bolliger <pedrobolliger@gmail.com>
2024-05-20 15:47:39 -0300

    new edits

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,5 @@
 class GenresController < ApplicationController
+  before_action :set_genre, only: [:edit, :update]
   def new
     @genre = Genre.new
     authorize @genre
@@ -10,9 +11,24 @@ class GenresController < ApplicationController
     @genre.save
   end
 
+  def edit
+    authorize @genre
+  end
+
+  def update
+    authorize @genre
+    @genre.update(genre_params)
+
+    redirect_to dashboard_path
+  end
+
   private
 
   def genre_params
     params.require(:genre).permit(:name)
+  end
+
+  def set_genre
+    @genre = genre.find(params[:id])
   end
 end
