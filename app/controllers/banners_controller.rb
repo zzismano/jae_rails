749e32d9b03212f8b377e68fcf7d8d1c54212c3e
Carla Valdivia <valdivia.carla@gmail.com>
2024-05-28 18:02:51 -0300

    Add banner create action && validation

--- app/controllers/banners_controller.rb
+++ app/controllers/banners_controller.rb
@@ -1,4 +1,5 @@
 class BannersController < ApplicationController
+  before_action :set_banner, only: %i[show edit update destroy]
 
   def index
   end
@@ -12,9 +13,18 @@ class BannersController < ApplicationController
   end
 
   def create
+    @banner = Banner.new(banner_params)
+    authorize @banner
+    if @banner.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+    
   end
 
   def edit
+    
   end
 
   def update
@@ -24,6 +34,10 @@ class BannersController < ApplicationController
   end
 
   private 
+
+  def set_banner
+    @banner = Banner.find(params[:id])
+  end
 
   def banner_params
     params.require(:banner).permit(:caption_one, :caption_two, :caption_three, :caption_four, :photo_one, :photo_two, :photo_three, :photo_four)
