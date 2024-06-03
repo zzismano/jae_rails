28391e070543c2177f47b7fca04bd6fff334ecee
Carla Valdivia <valdivia.carla@gmail.com>
2024-06-02 22:39:59 -0300

    Add calculate_date helper to format date and hour

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,7 @@
 module CardsHelper
+  def calculate_date(date)
+    weekdays = { 0 => 'Dom', 1 => 'Seg', 2 => 'Ter', 3 => 'Qua', 4 => 'Qui', 5 => 'Sex', 6 => 'Sab' }
+
+    return "#{weekdays[date.wday]}, #{date.mday}"
+  end
 end
