72c4939070d5f3e0f6ffb7091bb05cf9c571db78
Caio Centeio <CFCenteio@gmail.com>
2025-09-11 02:08:27 -0300

    Add hide_date feature to leisure events
    This commit introduces a new field 'hide_date' to the leisure model, allowing events to be automatically hidden after a specified date. The leisure_params method has been updated to permit this new attribute. Additionally, the edit and new views have been modified to include an input for 'hide_date', enhancing the user interface. The scheduler task has also been updated to hide events with an expired hide_date, improving event management.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_29_040429) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_11_041444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -134,6 +134,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_29_040429) do
     t.jsonb "dates", default: []
     t.string "alt_text"
     t.decimal "price", precision: 8, scale: 2
+    t.date "hide_date"
     t.index ["category_id"], name: "index_leisures_on_category_id"
     t.index ["user_id"], name: "index_leisures_on_user_id"
   end
