10a8483988033f6dcfbdc7e318c046a9440130b0
Caio Centeio <CFCenteio@gmail.com>
2025-09-21 19:52:16 -0300

    Implement price formatting helper for leisure events
    This commit introduces a new helper method `format_price` in the LeisuresHelper module to standardize the display of prices across various leisure event views. The method formats prices to show no decimal places for whole numbers and two decimal places for fractional values. All relevant views have been updated to utilize this new method, enhancing consistency in price presentation.

--- app/helpers/leisures_helper.rb
+++ app/helpers/leisures_helper.rb
@@ -2,5 +2,16 @@ module LeisuresHelper
   def partial_select(leisure)
     "shared/#{leisure.category.name.downcase}"
   end
+
+  def format_price(price)
+    # Check if the price has decimal places that are not zero
+    if price % 1 == 0
+      # No decimal places needed
+      number_with_precision(price, precision: 0, separator: ',', delimiter: '.')
+    else
+      # Show decimal places
+      number_with_precision(price, precision: 2, separator: ',', delimiter: '.')
+    end
+  end
 end
 
